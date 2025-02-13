e14089907a8e5bedae748351d5324b322dbf57fb
Shakir Ahmed Zahedi <shakirahmed84@yahoo.com>
2025-02-13 16:12:40 +0100

    Initialize the springboot project

---
+++ src/main/java/com/example/shoppingwithspringboot/ShoppingWithSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.shoppingwithspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingWithSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShoppingWithSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/shoppingwithspringboot/ShoppingWithSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shoppingwithspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingWithSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
